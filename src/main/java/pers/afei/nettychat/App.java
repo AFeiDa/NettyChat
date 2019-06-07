bb2abdc93c624bfcb58c86916d5e48e13cf34a36
AFei <super_afei@qq.com>
2019-06-07 20:31:34 +0800

    加入打包插件

--- src/main/java/pers/afei/nettychat/App.java
+++ src/main/java/pers/afei/nettychat/App.java
@@ -1,13 +1,26 @@
 package pers.afei.nettychat;
 
+import pers.afei.nettychat.client.SimpleChatClient;
+import pers.afei.nettychat.server.SimpleChatServer;
+
 /**
- * Hello world!
- *
+ * 这个类基本没用,或者说方便打包用的吧(打包客户端和服务端不用改pom)，顺便演示了一下执行客户端和服务端的代码~~~
+ * PATTERN为true则为服务端，为false则为客户端
+ * 启动服务端可以执行pers.afei.nettychat.server.SimpleChatServer类
+ * 启动客户端可以执行pers.afei.nettychat.client.SimpleChatClient类
  */
 public class App 
 {
-    public static void main( String[] args )
+    final static boolean PATTERN = false;
+    public static void main( String[] args ) throws Exception
     {
-        System.out.println( "Hello World!" );
+        final int port = 8080;
+
+        if(PATTERN) {
+            new SimpleChatServer(port).run();
+        } else {
+            new SimpleChatClient("localhost", port).run();
+        }
+        
     }
 }
